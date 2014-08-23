7d6692ad02483a881f04ade277b359c36a8300fa
coubann <coubann@gmail.com>
2014-08-23 17:55:05 +0800

    Revert "missulife.comtheme"
    This reverts commit 8f3b7851a327e33184e86fc87af0378a22a51aed.

--- sidebar-universal.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'universal' ) ) : ?>
-
-	<aside id="universal" class="sidebar" role="complementary">
-
-		<?php dynamic_sidebar( 'universal' ); ?>
-
-	</aside>
-
-<?php endif; ?>
